1c650aa3db4d9a3cc6eb9484fd3713f3e9a40139
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-09-07 19:21:34 -0700

    edit main page.tsx flash card to todo list

--- app/(routes)/dashboard/page.tsx
+++ app/(routes)/dashboard/page.tsx
@@ -3,7 +3,7 @@ export default function Page() {
   return (
     <div className="min-h-screen h-screen m-0 bg-stone-100">
       <div className="flex h-1/5 justify-center items-center mx-auto">
-        <h1 className="text-5xl">Welcome to Flash Card App</h1>
+        <h1 className="text-5xl">Welcome to ToDo List App</h1>
       </div>
     </div>
   );
