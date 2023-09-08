30c262adfadc2ea7fe8eb469cc72b7f8b2301935
daniel7842900 <kyungmin.dan.lim@gmail.com>
2023-09-07 19:20:20 -0700

    first commit

---
+++ app/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export default function Page() {
+  return (
+    <div className="min-h-screen h-screen m-0 bg-stone-100">
+      <div className="flex h-1/5 justify-center items-center mx-auto">
+        <h1 className="text-5xl">Welcome to Flash Card App</h1>
+      </div>
+    </div>
+  );
+}
